429b378cf31766f7244263d1a6520aef53cfa0fa
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2018-01-27 13:39:22 +0200

    set body color from player

---
+++ Assets/Scripts/BodyColorController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class BodyColorController : MonoBehaviour
+{
+	public Player Player;
+
+	SpriteRenderer spriteRenderer;
+	
+	void Awake()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	void Update()
+	{
+		spriteRenderer.color = Player.color;
+	}
+}
